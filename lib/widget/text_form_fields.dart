9a821c91675f888c59a14652b5e60d348072bbee
Castropy <randrescastroi@gmail.com>
2025-12-22 23:30:37 -0400

    Creando la carpeta de widgets donde guardaremos los archivos widgets reutilizables del codigo dando mas modularidad, robustez y haciendo el codigo mas escalable.

---
+++ lib/widget/text_form_fields.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormFields extends StatelessWidget {
+  const CustomTextFormFields({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  TextFormField();
+  }
+}
