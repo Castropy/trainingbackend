dfd33a1f899be3d7fee6502fdd5990c8c5b8fe35
Castropy <randrescastroi@gmail.com>
2025-12-23 01:08:47 -0400

    Logica del customTextFormFields

--- lib/widget/shared/text_form_fields.dart
+++ lib/widget/shared/text_form_fields.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormFields extends StatelessWidget {
-  const CustomTextFormFields({super.key});
+  final TextEditingController controller;
+  final String label;
+
+  const CustomTextFormFields({
+    super.key,
+    required this.controller,
+    required this.label,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return  TextFormField();
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(labelText: label),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "Este campo es obligatorio";
+        }
+        return null;
+      },
+    );
   }
 }
