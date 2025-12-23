278ba92f347b7bad4dd3f914c7b4e0c60b6b4cb6
Castropy <randrescastroi@gmail.com>
2025-12-22 23:28:16 -0400

    Creando la pantalla donde se mostraran los archivos guardados en el backend

---
+++ lib/screens/saved_form_screens.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SavedFormScreens extends StatelessWidget {
+  const SavedFormScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
